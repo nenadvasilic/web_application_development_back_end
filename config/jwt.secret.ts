b1bb7531f1ced7baf5bd68a12827d3fa7eccdbca
Nenad Vasilic <vasilic2013@gmail.com>
2022-02-09 16:43:03 +0100

    Implement the use of JWT

---
+++ config/jwt.secret.ts
@@ -0,0 +1,5 @@
+export const jwtSecret: string = '1qay2wsx3edc4rfv5tgb6zhn7ujm8ik9ol0p' +
+                                 '1234567890qwertzuiopasdfghjklyxcvbnm' +
+                                 'mnbvcxylkjhgfdsapoiuztrewq0987654321' +
+                                 'p0lo9ki8mju7nhz6bgt5vfr4cde3xsw2yaq1' +
+                                 '.ajdesadprovaliakomozesmamicutitvoju';
